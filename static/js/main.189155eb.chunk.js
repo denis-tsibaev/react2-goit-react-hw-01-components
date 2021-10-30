59d709e3c9ade28d20f7278d1510ccd38a7b064c
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-10-31 00:29:58 +0700

    Updates

--- static/js/main.189155eb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact2-goit-react-hw-01-components"]=this["webpackJsonpreact2-goit-react-hw-01-components"]||[]).push([[0],{11:function(t,e,c){"use strict";c.r(e);var n=c(1),r=c.n(n),o=c(3),s=c.n(o),a=(c(8),c(0));var i=function(){return Object(a.jsx)("div",{className:"App",children:"123"})};c(10);s.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(t,e,c){}},[[11,1,2]]]);
-//# sourceMappingURL=main.189155eb.chunk.js.map
